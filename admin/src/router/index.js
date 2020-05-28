f60077678d8b4f386f7b90d55994ff7148a8d2ac
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-05-28 20:58:06 +0800

    物品的CRUD

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,38 +1,52 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Main from '../views/Main.vue'
-import CategoryEdit from '../views/CategoryEdit'
-import CategoryList from '../views/CategoryList'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Main from "../views/Main.vue";
+import CategoryEdit from "../views/CategoryEdit";
+import CategoryList from "../views/CategoryList";
 
+import ItemEdit from "../views/ItemEdit";
+import ItemList from "../views/ItemList";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'main',
+    path: "/",
+    name: "main",
     component: Main,
-    children:[
+    children: [
       {
-        path:'/categories/create',
-        component: CategoryEdit
-      },
-      {
-        path:'/categories/edit/:id',
+        path: "/categories/create",
         component: CategoryEdit,
-        props: true
       },
       {
-        path:'/categories/list',
-        component: CategoryList
+        path: "/categories/edit/:id",
+        component: CategoryEdit,
+        props: true,
       },
-    ]
+      {
+        path: "/categories/list",
+        component: CategoryList,
+      },
+      {
+        path: "/items/create",
+        component: ItemEdit,
+      },
+      {
+        path: "/items/edit/:id",
+        component: ItemEdit,
+        props: true,
+      },
+      {
+        path: "/items/list",
+        component: ItemList,
+      },
+    ],
   },
-
-]
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
