abe40bcbbb071e88449eafd8db45b445fa7d12e3
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-05-31 01:32:37 +0800

    aaa

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -2,11 +2,6 @@
   <div class="about">
     <h1>{{id? '編輯':'新建'}}物品</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-      <!-- <el-form-item label="上級分類">
-        <el-select v-model="model.parent">
-          <el-option v-for="item in parents" :key="item._id" :label="item.name" :value="item._id"></el-option>
-        </el-select>
-      </el-form-item>-->
       <el-form-item label="名稱">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
