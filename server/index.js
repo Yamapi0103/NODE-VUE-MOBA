f7a0ef41eb027b3737e69d481e354877c27f31dd
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-07-17 14:43:16 +0800

    P1 4-1 生產環境編譯

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
 
@@ -6,12 +6,14 @@ app.set('secret', '34f34rt53q4t')
 
 app.use(require('cors')())
 app.use(express.json())
+app.use('/', express.static(__dirname + '/web'))
+app.use('/admin', express.static(__dirname + '/admin'))
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
 require('./plugins/db')(app)
 require('./routes/admin')(app)
 require('./routes/web')(app)
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log('http://localhost:3000/')
 })
