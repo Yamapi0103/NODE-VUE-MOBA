bfbd703be82dcd1284efcead666eb6bd3a904d7e
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-07-20 12:20:25 +0800

    修改md

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # NODE-VUE-MOBA
-後台: admin 
-伺服器:server 
-前台: web
+後台: admin/ 
+伺服器:server/ 
+前台: web/
 
-在對應資料夾底下執行npm run serve，即可開啟對應的程式碼
+執行步驟
+在server資料夾下安裝所需資源(npm i)
+執行npm run serve，即可開啟
 
 教學視頻:
 https://www.bilibili.com/video/BV1A4411Y7fi
@@ -11,20 +13,4 @@ UP主github:
 https://github.com/topfullstack/node-vue-moba
 
 
-###小技巧
-抓新聞資訊:
-$$('.news_list .title').map(el => el.innerHTML) 
-抓英雄分類
-$$('.hero-nav > li').map(li=>li.innerText)
-抓英雄分類對應的細項
-$$(".hero-nav > li").map((li, i) => {
-  return {
-    name: li.innerText,
-    heroes: $$("li", $$(".hero-list")[i]).map((el) => {
-      return {
-        name: $$("h3", el)[0].innerHTML,
-        avatar:$$('img', el)[0].src
-      };
-    }),
-  };
-});
+
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,101 +1,102 @@
 module.exports = (app) => {
-  const express = require("express");
-  const jwt = require("jsonwebtoken");
-  const assert = require("http-assert");
-  const AdminUser = require("../../models/AdminUser");
+  const express = require('express')
+  const jwt = require('jsonwebtoken')
+  const assert = require('http-assert')
+  const AdminUser = require('../../models/AdminUser')
 
   const router = express.Router({
     mergeParams: true,
-  });
+  })
   // 創建資源
-  router.post("/", async (req, res) => {
-    const model = await req.Model.create(req.body);
-    res.send(model);
-  });
+  router.post('/', async (req, res) => {
+    const model = await req.Model.create(req.body)
+    res.send(model)
+  })
   // 更新資源
-  router.put("/:id", async (req, res) => {
-    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
-    res.send(model);
-  });
+  router.put('/:id', async (req, res) => {
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
+    res.send(model)
+  })
   // 刪除資源
-  router.delete("/:id", async (req, res) => {
-    await req.Model.findByIdAndDelete(req.params.id);
+  router.delete('/:id', async (req, res) => {
+    await req.Model.findByIdAndDelete(req.params.id)
     res.send({
       success: true,
-    });
-  });
+    })
+  })
   // 資源列表
-  router.get("/", async (req, res) => {
-    const queryOptions = {};
-    if (req.Model.modelName === "Category") {
-      queryOptions.populate = "parent";
+  router.get('/', async (req, res) => {
+    const queryOptions = {}
+    if (req.Model.modelName === 'Category') {
+      queryOptions.populate = 'parent'
     }
     // const items = await req.Model.find().populate('parent').limit(10)
-    const items = await req.Model.find().setOptions(queryOptions).limit(100);
+    const items = await req.Model.find().setOptions(queryOptions).limit(100)
 
-    res.send(items);
-  });
+    res.send(items)
+  })
   // 資源詳情
-  router.get("/:id", async (req, res) => {
-    const model = await req.Model.findById(req.params.id);
-    res.send(model);
-  });
-  const authMiddleware = require("../../middleware/auth");
+  router.get('/:id', async (req, res) => {
+    const model = await req.Model.findById(req.params.id)
+    res.send(model)
+  })
+  const authMiddleware = require('../../middleware/auth')
   // 獲取模型middleware
-  const resourceMiddleware = require("../../middleware/resource");
+  const resourceMiddleware = require('../../middleware/resource')
   app.use(
-    "/admin/api/rest/:resource",
+    '/admin/api/rest/:resource',
     authMiddleware(),
     resourceMiddleware(),
     router
-  );
+  )
 
-  const multer = require("multer");
-  var path = require("path");
+  const multer = require('multer')
+  var path = require('path')
   // const upload = multer({ dest: path.join(__dirname, "/../../uploads") }); // __dirname:文件當前目錄
   var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "/../../uploads"));
+      cb(null, path.join(__dirname, '/../../uploads'))
     },
     // 將filename由隨機變數改為originalname(上傳圖片的檔名)
     filename: function (req, file, cb) {
-      cb(null, file.originalname);
+      cb(null, file.originalname)
     },
-  });
-  var upload = multer({ storage: storage });
+  })
+  var upload = multer({ storage: storage })
 
   app.post(
-    "/admin/api/upload",
+    '/admin/api/upload',
     authMiddleware(),
-    upload.single("file"),
+    upload.single('file'),
     async (req, res) => {
-      const file = req.file;
-      file.url = `http://localhost:3000/uploads/${file.filename}`;
-      res.send(file);
+      const file = req.file
+      file.url = `http://localhost:3000/uploads/${file.filename}` 
+      // 上線後 需改成線上域名 ex: http://test.tipfullstack.com/uploads...
+      res.send(file)
     }
-  );
+  )
 
-  app.post("/admin/api/login", async (req, res) => {
-    const { username, password } = req.body;
+  app.post('/admin/api/login', async (req, res) => {
+    const { username, password } = req.body
     // 1.根據用戶找名找用戶
-    const user = await AdminUser.findOne({ username }).select("+password");
+    const user = await AdminUser.findOne({ username }).select('+password')
 
-    assert(user, 422, "用戶不存在");
+    assert(user, 422, '用戶不存在')
 
     // 2.校驗密碼
-    const isValid = require("bcrypt").compareSync(password, user.password);
+    const isValid = require('bcrypt').compareSync(password, user.password)
 
-    assert(isValid, 422, "密碼錯誤");
+    assert(isValid, 422, '密碼錯誤')
 
     // 3.返回token
-    const token = jwt.sign({ id: user._id }, app.get("secret"));
-    res.send({ token });
-  });
+    const token = jwt.sign({ id: user._id }, app.get('secret'))
+    res.send({ token })
+  })
 
   // 錯誤處理函數
   app.use(async (err, req, res, next) => {
     res.status(err.statusCode || 500).send({
       message: err.message,
-    });
-  });
-};
+    })
+  })
+}
